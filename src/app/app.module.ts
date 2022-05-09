0e1b988b7e31b6f2569fefe2e5c3ba8833ca8958
gimena sasso <94083786+Gime8807@users.noreply.github.com>
2022-05-09 20:42:44 -0300

    Creacion del componente skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     HeaderComponent,
     PerfilComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
